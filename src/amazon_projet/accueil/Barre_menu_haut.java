c37c40afe844b931b0c01fc7d8fd10f85baf4bb1
Abdel Benamara <abdelbenamara@macbook-pro-de-abdel.home>
2020-02-15 13:06:18 +0100

    Remaniement de la partie haute

--- src/amazon_projet/accueil/Barre_menu_haut.java
+++ src/amazon_projet/accueil/Barre_menu_haut.java
@@ -11,8 +11,8 @@ import javafx.scene.layout.RowConstraints;
 
 public class Barre_menu_haut extends GridPane {
 
-	public Barre_menu_haut() {                
-                // -------------------------------------------------------------------------------
+	public Barre_menu_haut() {
+		// -------------------------------------------------------------------------------
 		// On donne les bonnes tailles (en pourcentage) au cases du GridPane
 		// -------------------------------------------------------------------------------
 
@@ -25,41 +25,46 @@ public class Barre_menu_haut extends GridPane {
 		ColumnConstraints column1 = new ColumnConstraints();
 		column1.setPercentWidth(20);
 		ColumnConstraints column2 = new ColumnConstraints();
-		column2.setPercentWidth(20);
-                ColumnConstraints column3 = new ColumnConstraints();
-		column3.setPercentWidth(20);
-                ColumnConstraints column4 = new ColumnConstraints();
-		column4.setPercentWidth(20);
-                ColumnConstraints column5 = new ColumnConstraints();
-		column5.setPercentWidth(20);
-		this.getColumnConstraints().addAll(column1, column2, column3, column4, column5);
+		column2.setPercentWidth(40);
+		ColumnConstraints column3 = new ColumnConstraints();
+		column3.setPercentWidth(30);
+		ColumnConstraints column4 = new ColumnConstraints();
+		column4.setPercentWidth(10);
+//		ColumnConstraints column5 = new ColumnConstraints();
+//		column5.setPercentWidth(20);
+		this.getColumnConstraints().addAll(column1, column2, column3, column4);
 
 		// On crée nos différents composants du menu
-                Composant_menu_haut mon_compte = new Composant_menu_haut("Mon Compte", "../img/img_moncompte.png", ContentDisplay.LEFT, HPos.CENTER);
+		Composant_menu_haut mon_compte = new Composant_menu_haut("Mon Compte", "../img/img_moncompte.png",
+				ContentDisplay.LEFT, HPos.CENTER);
 
-                Composant_menu_haut panier = new Composant_menu_haut("Mon Panier", "../img/img_panier.png", ContentDisplay.LEFT, HPos.CENTER);
+//		Composant_menu_haut panier = new Composant_menu_haut("Mon Panier", "../img/img_panier.png", ContentDisplay.LEFT, HPos.CENTER);
 
-		ImageView img_logo = new ImageView(new Image(this.getClass().getResourceAsStream("../img/img_logo_amazon.png")));
+		ImageView img_logo = new ImageView(
+				new Image(this.getClass().getResourceAsStream("../img/img_logo_amazon.png")));
 		img_logo.setFitHeight(50);
-                img_logo.setFitWidth(150);
-                GridPane.setValignment(img_logo, VPos.CENTER);
-                GridPane.setHalignment(img_logo, HPos.CENTER);
+		img_logo.setFitWidth(150);
+		GridPane.setValignment(img_logo, VPos.CENTER);
+		GridPane.setHalignment(img_logo, HPos.CENTER);
 
-                Composant_menu_haut ventes_speciales = new Composant_menu_haut("Ventes spéciales", "../img/img_ventesspeciales.png", ContentDisplay.RIGHT, HPos.CENTER);
-                
-                Composant_menu_haut menu_deroulant_droite = new Composant_menu_haut("   ", "../img/img_menuderoulantdroite.png", ContentDisplay.LEFT, HPos.RIGHT);
-                
+		Composant_menu_haut ventes_speciales = new Composant_menu_haut("Ventes spéciales",
+				"../img/img_ventesspeciales.png", ContentDisplay.RIGHT, HPos.CENTER);
+
+		Composant_menu_haut menu_deroulant_droite = new Composant_menu_haut("   ", "../img/img_menuderoulantdroite.png",
+				ContentDisplay.LEFT, HPos.RIGHT);
+
 		// On attache tous les composants au conteneur du haut
-		//this.getChildren().addAll(mon_compte, panier, img_logo);
-                this.add(mon_compte, 0, 0);
-                this.add(panier, 1, 0);
-                this.add(img_logo, 2, 0);
-                this.add(ventes_speciales, 3, 0);
-                this.add(menu_deroulant_droite, 4, 0);
-                
-                //this.setMaxHeight(Double.MAX_VALUE);
-                //this.setMaxWidth(Double.MAX_VALUE);
-                this.setStyle("-fx-background-color: #183152;");
-            
+		// this.getChildren().addAll(mon_compte, panier, img_logo);
+		this.add(mon_compte, 0, 0);
+//		this.add(panier, 1, 0);
+		this.add(img_logo, 1, 0);
+		this.add(ventes_speciales, 2, 0);
+		this.add(menu_deroulant_droite, 3, 0);
+		// this.setGridLinesVisible(true);
+
+		// this.setMaxHeight(Double.MAX_VALUE);
+		// this.setMaxWidth(Double.MAX_VALUE);
+		this.setStyle("-fx-background-color: #183152;");
+
 	}
 }
--- src/amazon_projet/accueil/Coin_haut_gauche.java
+++ src/amazon_projet/accueil/Coin_haut_gauche.java
@@ -1,19 +1,23 @@
 package amazon_projet.accueil;
 
-import javafx.geometry.Insets;
-import javafx.scene.control.Label;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.layout.HBox;
 
-public class Coin_haut_gauche extends Label {
+public class Coin_haut_gauche extends HBox {
 
 	public Coin_haut_gauche() {
-		this.setText("Vos commandes");
-		this.setFont(new Font("Comic sans MS", 30));
+//		this.setText("Vos commandes");
+//		this.setFont(new Font("Comic sans MS", 30));
+//		this.setStyle("-fx-font-weight: bold; -fx-background-color: #183152;");
+//		this.setPadding(new Insets(10));
+//		this.setTextFill(Color.WHITE);
+//		this.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		
+		Composant_menu_haut panier = new Composant_menu_haut("Mon Panier", "../img/img_panier.png", ContentDisplay.LEFT, HPos.CENTER);
+		this.getChildren().add(panier);
+		this.setAlignment(Pos.CENTER);
 		this.setStyle("-fx-font-weight: bold; -fx-background-color: #183152;");
-		this.setPadding(new Insets(10));
-                this.setTextFill(Color.WHITE);
-                this.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                
 	}
 }
