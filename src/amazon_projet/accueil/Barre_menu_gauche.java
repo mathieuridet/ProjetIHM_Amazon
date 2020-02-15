ecf10d4420d6136b33b4ff1fde2b07836397f689
Abdel Benamara <abdelbenamara@macbook-pro-de-abdel.home>
2020-02-15 02:49:57 +0100

    Boutons "+" "-" menu gauche et icone 3 points en blanc et en png

--- src/amazon_projet/accueil/Barre_menu_gauche.java
+++ src/amazon_projet/accueil/Barre_menu_gauche.java
@@ -2,20 +2,29 @@ package amazon_projet.accueil;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class Barre_menu_gauche extends BorderPane {
-    
+
 	public Barre_menu_gauche() {
 		// menu_gauche.maxHeight(scene.getHeight());
 		// menu_gauche.minHeight(scene.getHeight());
@@ -33,31 +42,19 @@ public class Barre_menu_gauche extends BorderPane {
 				produit4, produit5);
 		ListView<VBox> articles_commande = new ListView<>(commandeList);
 		articles_commande.setOrientation(Orientation.VERTICAL);
-                articles_commande.setStyle("-fx-control-inner-background: #183152; -fx-background-insets: 0; -fx-padding: 0;");
+		articles_commande.setStyle("-fx-control-inner-background: #183152; -fx-background-insets: 0; -fx-padding: 0;");
 
 		// Prix et date livraison commande
 		Label prix_commande = new Label("Prix");
 		prix_commande.setFont(new Font("Comic sans MS", 20));
 		prix_commande.setStyle("-fx-font-weight: bold");
-                prix_commande.setTextFill(Color.WHITE);
+		prix_commande.setTextFill(Color.WHITE);
 		prix_commande.setPadding(new Insets(10));
 		Label datelivraison_commande = new Label("Date Livraison");
 		datelivraison_commande.setFont(new Font("Comic sans MS", 20));
 		datelivraison_commande.setStyle("-fx-font-weight: bold");
-                datelivraison_commande.setTextFill(Color.WHITE);
+		datelivraison_commande.setTextFill(Color.WHITE);
 		datelivraison_commande.setPadding(new Insets(10));
-
-		// On attache le prix et la date de livraison dans une VBox
-		VBox prixEtDate = new VBox();
-		prixEtDate.getChildren().add(prix_commande);
-		prixEtDate.getChildren().add(datelivraison_commande);
-		prixEtDate.setAlignment(Pos.CENTER);
-
-		// Résumé commande (regroupe liste d'articles, prix et date)
-		BorderPane commande = new BorderPane();
-                commande.setStyle("-fx-background-color: #183152;");
-		commande.setCenter(articles_commande);
-		commande.setBottom(prixEtDate);
 
 		// Icône en bas du menu de gauche
 		Image img_icone = new Image(this.getClass().getResourceAsStream("../img/img_icone.png"));
@@ -65,11 +62,53 @@ public class Barre_menu_gauche extends BorderPane {
 		imgview_icone.setFitHeight(75);
 		imgview_icone.setFitWidth(75);
 
+		// On attache le prix et la date de livraison dans une VBox
+		VBox prixEtDate = new VBox();
+		prixEtDate.getChildren().addAll(prix_commande, datelivraison_commande, imgview_icone);
+		prixEtDate.setAlignment(Pos.CENTER);
+		prixEtDate.setBorder(new Border(
+				new BorderStroke(Color.WHITE, BorderStrokeStyle.SOLID, new CornerRadii(2), null, new Insets(8))));
+		prixEtDate.setVisible(false);
+		prixEtDate.setManaged(false);
+
+		// Résumé commande (regroupe liste d'articles, prix et date)
+		BorderPane commande = new BorderPane();
+		commande.setStyle("-fx-background-color: #183152;");
+		commande.setCenter(articles_commande);
+		commande.setBottom(prixEtDate);
+
+		// partie pour afficher et cacher la partie basse
+		Button cacher = new Button("Moins ( - )");
+		cacher.setAlignment(Pos.CENTER);
+		cacher.setMaxWidth(Double.MAX_VALUE);
+		cacher.setOnMousePressed(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent me) {
+				prixEtDate.setVisible(false);
+				prixEtDate.setManaged(false);
+			}
+		});
+
+		Button voir = new Button("Plus ( + )");
+		voir.setAlignment(Pos.CENTER);
+		voir.setMaxWidth(Double.MAX_VALUE);
+		voir.setOnMousePressed(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent me) {
+				prixEtDate.setVisible(true);
+				prixEtDate.setManaged(true);
+			}
+		});
+
+		HBox boutons_bas = new HBox();
+		boutons_bas.getChildren().addAll(cacher, voir);
+		HBox.setHgrow(cacher, Priority.ALWAYS);
+		HBox.setHgrow(voir, Priority.ALWAYS);
+
 		// On attache la commande au centre du menu de gauche
 		this.setCenter(commande);
+		this.setBottom(boutons_bas);
 		// On attache l'icone en bas du menu
-		this.setBottom(imgview_icone);
-                this.setStyle("-fx-background-color: #183152;");
+		// this.setBottom(imgview_icone);
+		this.setStyle("-fx-background-color: #183152;");
 		BorderPane.setAlignment(imgview_icone, Pos.CENTER);
 	}
 }
